da6d9bcb4585d17c16cef968adba95744df0d89a
jesuscruz12 <20221059@uthh.edu.mx>
2023-05-30 15:13:12 -0600

    commit

---
+++ Js/incio.js
@@ -0,0 +1,15 @@
+var v1,v2,res
+
+function captura1() {
+    v1=prompt("captura el valor 1")
+    v1=Number.parseInt(v1)
+    
+}
+
+function captura2() {
+    v2=Number.parseInt( prompt("captura el valor 2"))
+}
+
+function suma(){
+    console.log("el resultado de la suma es :"+(v1+v2))
+}
