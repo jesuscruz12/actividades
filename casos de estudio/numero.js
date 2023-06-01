337f42af4cb8e404a841c88e5c87a50a1218daed
jesuscruz12 <20221059@uthh.edu.mx>
2023-06-01 15:19:31 -0600

    commit

---
+++ casos de estudio/numero.js
@@ -0,0 +1,22 @@
+var adivina=Math.floor(Math.random()*100)+1;//genera un numero aleatorio entre 1 y 100
+var contador=0;
+function checkGuess(){
+    var valor=parseInt(document.getElementById('guessInput').value);
+
+    if(isNaN(valor)){
+        document.getElementById('result').innerHTML='por favor ingrese un numero valido';
+    }else{
+    contador++;
+
+
+        if (valor===adivina){
+        document.getElementById('result').innerHTML='Numero correcto es '+adivina+' en '+contador+' intentos';
+        }else if(valor<adivina){
+        document.getElementById('result').innerHTML='Intenta con un numero mas grande';
+        }
+        else{
+        document.getElementById('result').innerHTML='Intenta con un numero mas pequeño';
+        }
+    }
+}
+
